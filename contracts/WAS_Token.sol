63df48c1a375596dc78ada7866fdd73f0e833ce9
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-27 19:42:35 +0300

    Base token

--- contracts/WAS_Token.sol
+++ contracts/WAS_Token.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.4.24;
 
 
-import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 
  
-contract WAS_Token {
-    
+contract WAS_Token is MintableToken, DetailedERC20 {
+    uint256 public totalSupplyLimit = 260260676;
+
+    constructor() DetailedERC20("Wasi", "WAS", 0) public {
+
+    }
+
+    function burnUnsoldTokens() public onlyOwner {
+        balances[owner] = 0;
+    }
 }
