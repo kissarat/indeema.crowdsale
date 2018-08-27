acb05ac115fe9cf9259eb2f2b631ebc9e7199116
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-27 21:38:05 +0300

    fixed typo

--- migrations/2_deploy_crowdsale.js
+++ migrations/2_deploy_crowdsale.js
@@ -7,14 +7,13 @@ module.exports = function (deployer, network, accounts) {
     //  TODO: change before deploy
     const RATE = 100;
     const WALLET = accounts[9];
-    let timigs = [web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.minutes(1), web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.weeks(1)];
-
+    let timings = [web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.minutes(1), web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.weeks(1)];
     //  TODO: change before deploy
 
     deployer.deploy(WAS_Token).then(async () => {
         let token = await WAS_Token.deployed();
 
-        await deployer.deploy(WAS_Crowdsale, RATE, WALLET, token.address, timigs);
+        await deployer.deploy(WAS_Crowdsale, RATE, WALLET, token.address, timings);
         let crowdsale = await WAS_Crowdsale.deployed();
         await token.transferOwnership(crowdsale.address);
         await crowdsale.mintTotalSupply();
