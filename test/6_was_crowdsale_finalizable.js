0d1695b17ff0beb28af16bc65646cdda23aa20d5
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-28 20:55:34 +0300

    Finalizable test fixed after changes

--- test/6_was_crowdsale_finalizable.js
+++ test/6_was_crowdsale_finalizable.js
@@ -8,87 +8,107 @@ import mockCrowdsale from "./helpers/mocks/mockCrowdsale";
 
 import expectThrow from './helpers/expectThrow';
 import {
-  advanceBlock
+    advanceBlock
 } from './helpers/advanceToBlock';
 import increaseTime, {
-  duration,
-  increaseTimeTo
+    duration,
+    increaseTimeTo
 } from './helpers/increaseTime';
 import latestTime from './helpers/latestTime';
 
 contract("Finalizable", (accounts) => {
-  let token;
-  let crowdsale;
-  let whitelisted_1 = accounts[1];
-  const TEAM_WALLET = accounts[8];
+    let token;
+    let crowdsale;
+    const WHITELISTED_1 = accounts[1];
+    const TEAM_WALLET = accounts[8];
 
-  let mockCrowdsaleData = mockCrowdsale();
-  let timings;
+    let openingTimings = [];
+    let closingTimings = [];
 
-  beforeEach("setup", async () => {
-    await advanceBlock();
+    beforeEach("setup", async () => {
+        await advanceBlock();
 
-    const WALLET = accounts[9];
-    timings = [latestTime() + duration.minutes(1), latestTime() + duration.weeks(1)];
+        const RATES_ETH = [300, 500]; //  tokens per ETH
+        const WALLET = accounts[9];
 
-    token = await WAS_Token.new();
-    crowdsale = await WAS_Crowdsale.new(mockCrowdsaleData.rate, WALLET, token.address, timings);
-    await token.transferOwnership(crowdsale.address);
-    await crowdsale.mintTotalSupplyAndTeam(TEAM_WALLET);
+        for (let i = 0; i < 2; i++) {
+            if (i == 0) {
+                openingTimings[i] = latestTime() + duration.minutes(1);
+            } else {
+                openingTimings[i] = closingTimings[i - 1] + 1;
+            }
 
-    //  increase time to open
-    increaseTimeTo(timings[0] + duration.minutes(1));
-    assert.isTrue(await crowdsale.hasOpened.call(), "crowdsale should be open on beforeEach");
-  });
+            closingTimings[i] = openingTimings[i] + duration.weeks(1);
+        }
 
-  describe("finalizing by owner", () => {
-    it("should validate owner can not finalize while not closed", async () => {
-      await expectThrow(crowdsale.finalize(), "owner can not finalize while not closed");
+        token = await WAS_Token.new();
+        crowdsale = await WAS_Crowdsale.new(WALLET, token.address, RATES_ETH, openingTimings, closingTimings);
+        await token.transferOwnership(crowdsale.address);
+        await crowdsale.mintTotalSupplyAndTeam(TEAM_WALLET);
+
+        //  increase time to open
+        increaseTimeTo(openingTimings[0] + duration.minutes(1));
+        assert.isTrue(await crowdsale.hasOpened.call(), "crowdsale should be open on beforeEach");
+
+        //  add to whitelist
+        await crowdsale.addAddressToWhitelist(WHITELISTED_1);
+        assert.isTrue(await crowdsale.whitelist.call(WHITELISTED_1), "WHITELISTED_1 should be whitelisted on beforeEach");
     });
 
-    it("should validate owner can finalize when closed", async () => {
-      //  increase time to close
-      increaseTimeTo(timings[1] + duration.minutes(1));
+    describe("finalizing by owner", () => {
+        it("should validate owner can not finalize while not closed", async () => {
+            await expectThrow(crowdsale.finalize(), "owner can not finalize while not closed");
+        });
 
-      //  finalize
-      await crowdsale.finalize();
-    });
-  });
+        it("should validate owner can finalize when closed", async () => {
+            //  increase time to close
+            increaseTimeTo(closingTimings[closingTimings.length - 1] + duration.minutes(1));
 
-  describe("finalizing by not owner", () => {
-    it("should validate not owner can not finalize while not closed", async () => {
-      await expectThrow(crowdsale.destroy({
-        from: accounts[1]
-      }), "not owner can not finalize while not closed");
+            //  finalize
+            await crowdsale.finalize();
+        });
     });
 
-    it("should validate not owner can not finalize if closed", async () => {
-      //  increase time to close
-      increaseTimeTo(timings[1] + duration.minutes(1));
+    describe("finalizing by not owner", () => {
+        it("should validate not owner can not finalize while not closed", async () => {
+            await expectThrow(crowdsale.destroy({
+                from: accounts[1]
+            }), "not owner can not finalize while not closed");
+        });
 
-      await expectThrow(crowdsale.finalize({
-        from: accounts[1]
-      }), "not owner can not finalize if closed");
+        it("should validate not owner can not finalize if closed", async () => {
+            //  increase time to close
+            increaseTimeTo(closingTimings[closingTimings.length - 1] + duration.minutes(1));
+
+            await expectThrow(crowdsale.finalize({
+                from: accounts[1]
+            }), "not owner can not finalize if closed");
+        });
     });
-  });
 
-  describe("tokens burning", () => {
-    it("should validate tokens are burnt", async () => {
-      console.log(new BigNumber(await token.balanceOf(crowdsale.address)).toNumber());
-      // await crowdsale.sendTransaction({
-      //   from: whitelisted_1,
-      //   value: web3.toWei(10, "ether")
-      // });
-      // let tokensBeforeFinalize = new BigNumber(await token.balanceOf(crowdsale.address));
-      // console.log(new BigNumber(await token.balanceOf(crowdsale.address)).toNumber());
+    describe.only("tokens tansferred to crowdsale owner balance", () => {
+        it("should validate tokens tansferred to crowdsale owner balance", async () => {
+            await crowdsale.sendTransaction({
+                from: WHITELISTED_1,
+                value: web3.toWei(1, "ether")
+            });
 
-      // //  increase time to close
-      // increaseTimeTo(timings[1] + duration.minutes(1));
+            let crowdsaleOwner = await crowdsale.owner.call();
+            let crowdsaleTokensBeforeFinalize = new BigNumber(await token.balanceOf(crowdsale.address));
+            let crowdsaleOwnerTokensBeforeFinalize = new BigNumber(await token.balanceOf(crowdsaleOwner));
+            assert.equal(crowdsaleOwnerTokensBeforeFinalize.toNumber(), 0, "crowdsale owner should not own tokens");
 
-      // //  finalize
-      // await crowdsale.finalize();
+            //  increase time to close
+            increaseTimeTo(closingTimings[closingTimings.length - 1] + duration.minutes(1));
 
-      // console.log(new BigNumber(await token.balanceOf(crowdsale.address)));
+            //  finalize
+            await crowdsale.finalize();
+
+            let crowdsaleTokensAfterFinalize = new BigNumber(await token.balanceOf(crowdsale.address));
+            let crowdsaleOwnerTokensAfterFinalize = new BigNumber(await token.balanceOf(crowdsaleOwner));
+
+            assert.equal(crowdsaleTokensAfterFinalize.toNumber(), 0, "crowdsale balance should be 0 tokens after finalization");
+            assert.equal(crowdsaleOwnerTokensAfterFinalize.toNumber(), crowdsaleTokensBeforeFinalize.toNumber(), "crowdsale tokens should be transferred to crowdsale owner balance");
+        });
     });
-  });
 });
