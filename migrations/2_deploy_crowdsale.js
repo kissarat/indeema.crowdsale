17ec9eb6606d20b80487663b2272f90bd49c4de9
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-28 19:42:23 +0300

    updated deployment

--- migrations/2_deploy_crowdsale.js
+++ migrations/2_deploy_crowdsale.js
@@ -5,16 +5,27 @@ let IncreaseTime = require("../test/helpers/increaseTime.js");
 
 module.exports = function (deployer, network, accounts) {
     //  TODO: change before deploy
-    const RATE = 100;
+    const RATES_ETH = [300, 500]; //  tokens per ETH
     const WALLET = accounts[9];
     const TEAM_WALLET = accounts[8];
-    let timings = [web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.minutes(1), web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.weeks(1)];
+    let openingTimings = [];
+    let closingTimings = [];
+
+    for (let i = 0; i < 2; i++) {
+        if (i == 0) {
+            openingTimings[i] = web3.eth.getBlock("latest").timestamp + IncreaseTime.duration.minutes(1);
+        } else {
+            openingTimings[i] = closingTimings[i - 1] + 1;
+        }
+
+        closingTimings[i] = openingTimings[i] + IncreaseTime.duration.weeks(1);
+    }
     //  TODO: change before deploy
 
     deployer.deploy(WAS_Token).then(async () => {
         let token = await WAS_Token.deployed();
 
-        await deployer.deploy(WAS_Crowdsale, RATE, WALLET, token.address, timings);
+        await deployer.deploy(WAS_Crowdsale, WALLET, token.address, RATES_ETH, openingTimings, closingTimings);
         let crowdsale = await WAS_Crowdsale.deployed();
         //  1
         await token.transferOwnership(crowdsale.address);
