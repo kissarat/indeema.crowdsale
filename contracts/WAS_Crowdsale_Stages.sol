f9f7cb5f0ebcb52c15844d45381bbf0cc9399c7b
Ivan Solomichev <vanijatko.social@gmail.com>
2018-08-29 18:44:13 +0300

    Added comments to tested methods

--- contracts/WAS_Crowdsale_Stages.sol
+++ contracts/WAS_Crowdsale_Stages.sol
@@ -21,17 +21,30 @@ contract WAS_Crowdsale_Stages is WAS_CrowdsaleReservations, Ownable {
     stagePurchaseTokensMinimum = [0, 100];
   }
 
+  /**
+   * @dev Determines rateETH for current stage.
+   * @return Current rateETH
+   */
   function currentRateETH() public view returns (uint256) {
     uint256 stage = currentStage();
     return rateETH[stage];
   }
 
+  /**
+   * @dev Determines discount for current stage.
+   * @param _tokenPurchasedAmount Token amount based on rate ETH (base token amount)
+   * @return Current discount
+   */
   function currentDiscount(uint256 _tokenPurchasedAmount) public view returns (uint256) {
     uint256 stage = currentStage();
 
     return discountPercentForStage(stage, _tokenPurchasedAmount);
   }
 
+  /**
+   * @dev Determines current stage. Throws if no stage is currently open.
+   * @return Current stage
+   */
   function currentStage() public view returns (uint256) {
     for (uint256 i = 0; i < closingTimings.length; i ++) {
       if (block.timestamp <= closingTimings[i]) {
@@ -59,12 +72,19 @@ contract WAS_Crowdsale_Stages is WAS_CrowdsaleReservations, Ownable {
   /**
    *  PRIVATE
    */
+
+   /**
+   * @dev Validates rates.
+   */
   function validateRates(uint256[] _rateETH) private pure {
     for (uint256 i = 0; i < _rateETH.length; i ++) {
       require(_rateETH[i] > 0, "rate must be > 0");
     }
   }
   
+  /**
+   * @dev Validates opening and closing times.
+   */
   function validateOpeningAndClosingTimings(uint256[] _openingTimings, uint256[] _closingTimings) private view {
     require(_openingTimings.length == _closingTimings.length, "timings length differs");
     require(_openingTimings[0] > block.timestamp, "should open in future");
@@ -78,6 +98,12 @@ contract WAS_Crowdsale_Stages is WAS_CrowdsaleReservations, Ownable {
     }
   }
 
+  /**
+   * @dev Determines discount for current stage based on parameters.
+   * @param _stage Current stage idx
+   * @param _tokenPurchasedAmount Tokens to be purchased for rateETH
+   * @return Current discount
+   */
   function discountPercentForStage(uint256 _stage, uint256 _tokenPurchasedAmount) private pure returns (uint256) {
     if (_stage == 0) {
       return 30;
